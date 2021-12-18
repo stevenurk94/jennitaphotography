c536cac41d60638e6c6317e9c7ca55342cfd728a
stevenurk94 <86805532+stevenurk94@users.noreply.github.com>
2021-12-18 18:30:01 +0100

    Add files via upload

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,20 +1,25 @@
 <template>
     <section>
-        <h2>Welkom op Jennita Photography</h2>
-        <br>
-        <p>Ik zal mij even voorstellen wie ik ben en hoe ik tot een webshop ben gekomen. Lees daarom snel verder en neem eens gerust een kijkje op mijn site.</p>
-        <h2>Wie ben ik?</h2>
-        <p>Ik ben Jennita Altena, woonachtig in Genemuiden. Ik wordt enthousiast van fotografie en wilde hier iets mee doen. Zo kwam ik op het idee om mijn foto's te verkopen als fotokaarten voor het goede doel. Ik wilde mij namelijk nuttig maken voor mensen die moeten missen wat ik hier in Nederland mag ontvangen. Het voelde voor mij als een geroepen plicht.</p>
-        <h2>Wanneer ben ik gestart?</h2>
-        <p>Vanaf 2020 ben ik gestart met mijn kaarten verkoop en heb ik mij bij 'Kom over en help' aangemeld als vrijwilliger. De reden dat ik voor 'Kom over en help' heb gekozen, is dat ik achter de visie sta en de organisatie graag wil steunen wat betreft de mensen die ik kwetsbare omstandigheden leven.</p>
-        <p>Helpt u ook mee om mensen die in kwetsbare omstandigheden leven een hoopvolle toekomst te geven? Dit kan door mijn fotocollectie te bekijken en uw bestelling te plaatsen.</p>
-        <br>
-        <p>Ik zie u graag</p>
-        <br>
-        <p>Liefs, Jennita</p>
-        <button class="button-1">Fotokaarten bestellen</button>
+        <div>
+            <h2>Welkom op Jennita Photography</h2>
+            <br>
+            <p>Ik zal mij even voorstellen wie ik ben en hoe ik tot een webshop ben gekomen. Lees daarom snel verder en neem eens gerust een kijkje op mijn site.</p>
+            <h2>Wie ben ik?</h2>
+            <p>Ik ben Jennita Altena, woonachtig in Genemuiden. Ik wordt enthousiast van fotografie en wilde hier iets mee doen. Zo kwam ik op het idee om mijn foto's te verkopen als fotokaarten voor het goede doel. Ik wilde mij namelijk nuttig maken voor mensen die moeten missen wat ik hier in Nederland mag ontvangen. Het voelde voor mij als een geroepen plicht.</p>
+            <h2>Wanneer ben ik gestart?</h2>
+            <p>Vanaf 2020 ben ik gestart met mijn kaarten verkoop en heb ik mij bij 'Kom over en help' aangemeld als vrijwilliger. De reden dat ik voor 'Kom over en help' heb gekozen, is dat ik achter de visie sta en de organisatie graag wil steunen wat betreft de mensen die ik kwetsbare omstandigheden leven.</p>
+            <p>Helpt u ook mee om mensen die in kwetsbare omstandigheden leven een hoopvolle toekomst te geven? Dit kan door mijn fotocollectie te bekijken en uw bestelling te plaatsen.</p>
+            <br>
+            <p>Ik zie u graag</p>
+            <br>
+            <p>Liefs, Jennita</p>
+            <button class="button-1">Fotokaarten bestellen</button>
+            
 
+        </div>
         <img src="~/assets/home-picture.jpg" alt="">
+
+
     </section>
 
     
@@ -31,20 +36,29 @@ h2 {
 
 
 section {
-    display: flex;
-    flex-direction: column;
-    
-    /* width: 80%; */
+    border: 1px solid #3a524db4;
     margin: 50px 0px;
+    border-radius: 5px;
+    padding: 10px;   
 }
 
-section button {
+section div {
+    max-width: 700px;
+    right: 0;
+    left: 0;
+    margin: auto;
+}
+
+section div button {
     margin-top: 50px;
 }
 
 section img {
     max-width: 250px;
     border-radius: 5px;
+    left: 0;
+    right: 0;
+    margin: auto;
 }
 
 </style>
