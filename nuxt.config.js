e03454209487d4e8c4aaf4ace6c401e84b6c9b68
stevenurk94 <86805532+stevenurk94@users.noreply.github.com>
2022-01-25 12:35:30 +0100

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/css/reset.css',
+    '~/assets/css/input.css',
     '~/assets/pageTransition.css',
   ],
 
