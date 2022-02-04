5ce6e0734c9a459ed20c3983cf06094c440c0959
stevenurk94 <86805532+stevenurk94@users.noreply.github.com>
2022-02-04 20:52:36 +0100

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="navbar">
 
-        <NuxtLink class="navbar__logo" to="/">JF</NuxtLink>
-        <!-- <NuxtLink class="nav__logo" to="/">
-            <img src="~/static/Nieuw logo l19.png" alt="">
+        <!-- <NuxtLink class="navbar__logo" to="/">JF</NuxtLink> -->
+        <NuxtLink class="navbar__logo" to="/">
+            <img src="~/static/Logo website.png" alt="">
         </NuxtLink>
-         -->
+        
 
         <input type="checkbox" id="toggle">
         <label for="toggle">
@@ -27,7 +27,6 @@
                     <NuxtLink to="/">Home
                         <span>&#8250;</span>
                     </NuxtLink>
-                    
                 </li>
                 <li>
                     <NuxtLink to="/kaarten">Kaarten bestellen
@@ -50,9 +49,9 @@
 
             <span v-if="cartItemCount" class="notification-label">{{ cartItemCount }}</span>
             <svg viewBox="0 0 38 38" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-                <g>
+                <!-- <g> -->
                     <path d="M19.1738378,3 C22.6918793,3 25.5613445,5.80614546 25.7099327,9.3242996 L25.7159769,9.61107428 L25.7158948,11.0581943 L28.2027404,11.0587642 C29.5869089,11.0587642 30.7593675,12.0623362 31.0031568,13.4192401 L31.0326705,13.6253444 L32.9831893,31.8135942 C33.1525648,33.3929902 32.0228677,34.8120964 30.4599416,34.9832561 C30.3580872,34.9944106 30.2557108,35 30.1532592,35 L30.1532592,35 L7.84649926,35 C6.27442109,35 5,33.7121503 5,32.1235069 C5,32.00032 5.00783077,31.8772604 5.02344396,31.7550878 L5.02344396,31.7550878 L7.34783519,13.5668381 C7.53113401,12.1325336 8.73975488,11.0587642 10.1708905,11.0587642 L10.1708905,11.0587642 L12.6312659,11.0581943 L12.6316986,9.61107428 C12.6316986,5.95987877 15.560714,3 19.1738378,3 Z M28.2027404,12.798039 L25.7158948,12.7974692 L25.7159769,18.897231 C25.7159769,19.3775185 25.3306868,19.7668684 24.8554074,19.7668684 C24.4233351,19.7668684 24.0656342,19.4450916 24.0041687,19.0257396 L23.9948378,18.897231 L23.9947557,12.7974692 L14.352405,12.7974692 L14.3528376,18.897231 C14.3528376,19.3775185 13.9675475,19.7668684 13.4922681,19.7668684 C13.0601959,19.7668684 12.702495,19.4450916 12.6410293,19.0257396 L12.6316986,18.897231 L12.6312659,12.7974692 L10.1708905,12.798039 C9.65224251,12.798039 9.20751456,13.1547483 9.08117693,13.6509732 L9.05479887,13.7896031 L6.73040763,31.9778528 C6.72423497,32.0261537 6.72113909,32.0748051 6.72113909,32.1235069 C6.72113909,32.7515753 7.22497995,33.2607251 7.84649926,33.2607251 L7.84649926,33.2607251 L30.1532592,33.2607251 L30.2745054,33.2541056 C30.8924068,33.1864376 31.3390312,32.6253957 31.2720689,32.0009834 L31.2720689,32.0009834 L29.32155,13.8127335 C29.2596437,13.235468 28.7773313,12.798039 28.2027404,12.798039 L28.2027404,12.798039 Z M19.1738378,4.73927488 C16.5971621,4.73927488 14.4926161,6.78200223 14.3595202,9.35233811 L14.3528376,9.61107428 L14.352405,11.0581943 L23.9948378,11.0581943 L23.9948378,9.61107428 C23.9948378,6.92045376 21.8364026,4.73927488 19.1738378,4.73927488 Z"></path>
-                </g>
+                <!-- </g> -->
             </svg>
         </NuxtLink>
 
@@ -92,7 +91,7 @@ header .navbar {
 /* ########## NAVBAR LOGO ########## */
 
 .navbar__logo {
-    font-family: 'Fleur De Leah', cursive;
+    /* font-family: 'Fleur De Leah', cursive;
     font-size: 40px;
     display: flex;
     justify-content: center;
@@ -101,19 +100,32 @@ header .navbar {
     cursor: pointer;
     z-index: 10;
     transition: padding 200ms ease-in-out;
-    color: white;
-    /* width: 80px; */
+    color: white; */
+    width: 205px;
 }
+
+header.scrolled .navbar__logo {
+    /* padding: 15px 0; */
+    color: black;
+    width: 200px;
+}
+
 
 .navbar__logo img {
     width: 100%;
     height: 100%;
+    filter: invert(1);
+    transition: all 150ms ease-in-out;
 }
 
-header.scrolled .navbar__logo {
-    padding: 15px 0;
-    color: black
+header.scrolled .navbar__logo img {
+    filter: invert(0);
+    margin: -5px 0 -5px 0;
+    width: 180px;
+
 }
+
+
 
 header.scrolled .navbar.open .navbar__logo {
     color: white;
