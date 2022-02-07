5876d9add427887f88df9aab44d4d0b59a379c16
stevenurk94 <86805532+stevenurk94@users.noreply.github.com>
2022-02-07 21:52:28 +0100

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -117,6 +117,9 @@ header.scrolled .navbar.open .navbar__logo img {
     header .navbar__logo {
         width: calc(2% + 140px);
     }
+    header .navbar__logo img, header.scrolled .navbar__logo img {
+        margin: 6px 0 5px 0;
+    }
 
 
 }
