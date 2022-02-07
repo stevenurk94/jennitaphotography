20989acb7b59d2eaa9c59af7e5975187d7736105
stevenurk94 <86805532+stevenurk94@users.noreply.github.com>
2022-02-07 21:45:10 +0100

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -14,9 +14,6 @@
         </label>
 
         <nav>
-            <!-- <div class="background-image"></div> -->
-            <!-- <div class="top-fade"></div> -->
-            <!-- <div class="left-fade"></div> -->
             <ul>
                 <li>
                     <h3>Menu</h3>
@@ -28,9 +25,6 @@
                 </li>
                 <li>
                     <NuxtLink to="/kaarten">Kaarten bestellen
-                        <!-- <svg class="menu-desktop__icon-button-list" width="7" height="10" viewBox="0 0 7 10" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1.243.757L5.485 5 1.243 9.243" stroke="currentColor" stroke-width="1.5" fill="none" fill-rule="evenodd"></path>
-                        </svg> -->
                         <span>&#8250;</span>
                     </NuxtLink>                   
                 </li>
@@ -45,7 +39,7 @@
 
         <NuxtLink class="cart" to="/winkelwagen">
 
-            <span v-if="cartItemCount" class="notification-label">{{ cartItemCount }}</span>
+            <span v-if="cartItemCount" class="cart__notification">{{ cartItemCount }}</span>
             <svg viewBox="0 0 38 38" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
                 <!-- <g> -->
                     <path d="M19.1738378,3 C22.6918793,3 25.5613445,5.80614546 25.7099327,9.3242996 L25.7159769,9.61107428 L25.7158948,11.0581943 L28.2027404,11.0587642 C29.5869089,11.0587642 30.7593675,12.0623362 31.0031568,13.4192401 L31.0326705,13.6253444 L32.9831893,31.8135942 C33.1525648,33.3929902 32.0228677,34.8120964 30.4599416,34.9832561 C30.3580872,34.9944106 30.2557108,35 30.1532592,35 L30.1532592,35 L7.84649926,35 C6.27442109,35 5,33.7121503 5,32.1235069 C5,32.00032 5.00783077,31.8772604 5.02344396,31.7550878 L5.02344396,31.7550878 L7.34783519,13.5668381 C7.53113401,12.1325336 8.73975488,11.0587642 10.1708905,11.0587642 L10.1708905,11.0587642 L12.6312659,11.0581943 L12.6316986,9.61107428 C12.6316986,5.95987877 15.560714,3 19.1738378,3 Z M28.2027404,12.798039 L25.7158948,12.7974692 L25.7159769,18.897231 C25.7159769,19.3775185 25.3306868,19.7668684 24.8554074,19.7668684 C24.4233351,19.7668684 24.0656342,19.4450916 24.0041687,19.0257396 L23.9948378,18.897231 L23.9947557,12.7974692 L14.352405,12.7974692 L14.3528376,18.897231 C14.3528376,19.3775185 13.9675475,19.7668684 13.4922681,19.7668684 C13.0601959,19.7668684 12.702495,19.4450916 12.6410293,19.0257396 L12.6316986,18.897231 L12.6312659,12.7974692 L10.1708905,12.798039 C9.65224251,12.798039 9.20751456,13.1547483 9.08117693,13.6509732 L9.05479887,13.7896031 L6.73040763,31.9778528 C6.72423497,32.0261537 6.72113909,32.0748051 6.72113909,32.1235069 C6.72113909,32.7515753 7.22497995,33.2607251 7.84649926,33.2607251 L7.84649926,33.2607251 L30.1532592,33.2607251 L30.2745054,33.2541056 C30.8924068,33.1864376 31.3390312,32.6253957 31.2720689,32.0009834 L31.2720689,32.0009834 L29.32155,13.8127335 C29.2596437,13.235468 28.7773313,12.798039 28.2027404,12.798039 L28.2027404,12.798039 Z M19.1738378,4.73927488 C16.5971621,4.73927488 14.4926161,6.78200223 14.3595202,9.35233811 L14.3528376,9.61107428 L14.352405,11.0581943 L23.9948378,11.0581943 L23.9948378,9.61107428 C23.9948378,6.92045376 21.8364026,4.73927488 19.1738378,4.73927488 Z"></path>
@@ -61,6 +55,7 @@
 
 <style>
 
+/* ########## HEADER SETTINGS ########## */
 header {
     width: 100%;
     top: 0;
@@ -85,48 +80,49 @@ header .navbar {
 
 
 
-
 /* ########## NAVBAR LOGO ########## */
-
-.navbar__logo {
-    /* font-family: 'Fleur De Leah', cursive;
-    font-size: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 25px 0;
-    cursor: pointer;
-    z-index: 10;
-    transition: padding 200ms ease-in-out;
-    color: white; */
-    width: 205px;
+header .navbar__logo {
+    width: calc(8% + 87px);
     z-index: 2;
 }
 
 header.scrolled .navbar__logo {
-    /* padding: 15px 0; */
     color: black;
     width: 200px;
 }
 
-
-.navbar__logo img {
+header .navbar__logo img {
     width: 100%;
     height: 100%;
     filter: invert(1);
     transition: all 150ms ease-in-out;
+    margin: 15px 0 14px 0;
+}
+
+header.scrolled .navbar__logo {
+    width: calc(5% + 87px)
 }
 
 header.scrolled .navbar__logo img {
     filter: invert(0);
-    margin: -5px 0 -5px 0;
-    width: 180px;
-
+    margin: 10px 0 8px 0;
 }
 
 header.scrolled .navbar.open .navbar__logo img {
     filter: invert(1);
 }
+
+
+@media only screen and (max-width: 900px) {
+    header .navbar__logo {
+        width: calc(2% + 140px);
+    }
+
+
+}
+
+
+
 
 
 
@@ -135,23 +131,10 @@ header.scrolled .navbar.open .navbar__logo img {
 
 
 /* ########## NAVBAR ITEMS ########## */
-
-
-
-
-
-
 header .navbar nav ul {
     display: flex;
 
 }
-/* header .navbar.open nav ul {
-    transform: translate(0, 0) scale(1) rotate(0deg);
-    opacity: 1;
-} */
-
-
-
 header .navbar nav ul li a {
     font-family: 'IBM Plex Serif', serif;
     color: rgba(255, 255, 255, 0.767);
@@ -198,63 +181,8 @@ header.scrolled .navbar nav ul li a::after {
 
 
 
-/* header .navbar nav ul li a svg {
-    transform: rotate(90deg);
-    margin-left: 3px;
-}
 
-header .navbar nav ul li a svg path {
-    fill: none;
-} */
-
-
-/* 
-header .navbar .background-image {
-    background: url("~/assets/img/kaart0004.jpeg");
-    background-size: cover;
-    background-position: center;
-    width: 100vw;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-} */
-
-/* header .navbar nav .top-fade {
-    width: 100vw;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    right: 0;
-    background: linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%);
-} */
-
-/* header .navbar nav .left-fade {
-    background: linear-gradient(270deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%);
-    position: absolute;
-    height: 100%;
-    width: 100vw;
-    right: 0;
-    top: 0;
-} */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* ### MENU BUTTON ### */
+/* ########## MENU BUTTON ########## */
 
 header .navbar input[type="checkbox"], header .navbar nav ul li a span, header .navbar nav ul li h3 {
   display: none;
@@ -265,8 +193,8 @@ header .navbar label {
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 25px;
-    width: 35px;
+    height: 20px;
+    width: 30px;
     cursor: pointer;
     position: relative;
     z-index: 999;
@@ -276,7 +204,7 @@ header .navbar label span {
     position: absolute;
     background-color: white;
     transition: var(--trans);
-    height: 2.5px;
+    height: 2px;
     width: 95%;
     border-radius: 50px;
     transition: all ease-in 250ms;
@@ -323,7 +251,7 @@ header .navbar input:checked ~ label span:nth-child(3) {
 
 
 
-@media only screen and (max-width: 850px) {
+@media only screen and (max-width: 900px) {
 
     header .navbar label {
         display: flex;
@@ -358,16 +286,17 @@ header .navbar input:checked ~ label span:nth-child(3) {
     }
 
     header .navbar nav ul {
-        padding-top: 33%;
+        padding-top: 140px;
         display: block;
         right: 0;
         left: 0;
         margin: auto;
         width: 95%;
+        max-width: 500px;
     }
 
     header .navbar nav ul li {
-        border-bottom: .5px solid rgba(255, 255, 255, 0.6);
+        border-bottom: .1px solid rgba(255, 255, 255, 0.25);
     }
 
 
@@ -378,15 +307,17 @@ header .navbar input:checked ~ label span:nth-child(3) {
         letter-spacing: 1.4px;
         text-align: center;
         margin-bottom: 13px;
+        font-size: 23px;
     }
 
     header .navbar nav ul li a {
         margin: 0;
-        padding: 14px 15px 15px 15px;  
+        padding: 9px 15px 11px 15px;  
         display: flex;
         justify-content: space-between;
         align-items: center;
         opacity: 0;
+        font-size: 17px;
     }
 
     header .navbar nav ul li:nth-child(1) h3 {
@@ -429,35 +360,6 @@ header .navbar input:checked ~ label span:nth-child(3) {
         font-weight: 400;
         padding-bottom: 2px;
     }
-
-
-
-
-
-
-
-
-    /* .navbar nav {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  transition: all .75s cubic-bezier(0.87, 0, 0.13, 1);
-  opacity: 0;
-  visibility: hidden;
-} */
-
-/* header .navbar.open nav {
-    visibility: visible;
-    opacity: 1;
-} */
-
 }
 
 
@@ -472,26 +374,24 @@ header .navbar input:checked ~ label span:nth-child(3) {
 
 .cart {
     position: relative;
-    width: 55px;
-    height: 55px;
-    padding: 5px;
+    width: 37px;
+    height: 37px;
 }
 
-.notification-label {
+.cart__notification {
     position: absolute;
-    top: 0;
-    right: 0;
+    top: -5px;
+    right: -5px;
     background: var(--clr-2-1);
-    border-radius: 100%;
-    width: 20px;
-    height: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: .75rem;
+    border-radius: 8px;
+    padding: 3px;
+    min-width: 18px;
+    height: 18px;
+    font-size: 12px;
     letter-spacing: 0;
+    line-height: 1;
     color: white;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: 'Work Sans', sans-serif;
     font-weight: 600;
     text-align: center;
 }
@@ -509,7 +409,6 @@ header.scrolled .navbar.open svg path {
     fill: white;
 }
 
-/*------------------------------------------*/
 
 </style>
 
