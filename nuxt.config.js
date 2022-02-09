5523939e685abff98002083528621b39e300bce1
stevenurk94 <86805532+stevenurk94@users.noreply.github.com>
2022-02-09 23:42:22 +0100

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,5 @@
 export default {
   // target: 'static',
-  // router: {
-  //   base: '/static-jennitaphotography/'
-  // },
   // ssr: 'false',
 
   // Transition between pages
@@ -23,30 +20,31 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/image00015.png' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fleur+De+Leah&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;500;600;700&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600&display=swap' },
     ],
 
     // Google Tag Manager - Configuration
-    __dangerouslyDisableSanitizers: ['script'],
-    script: [
-      {
-        charset: 'utf-8',
-        type: "text/javascript",
-        innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NWX2HBM');`
-      }
-    ],
+    // __dangerouslyDisableSanitizers: ['script'],
+    // script: [
+    //   {
+    //     charset: 'utf-8',
+    //     type: "text/javascript",
+    //     innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    //     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    //     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    //     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    //     })(window,document,'script','dataLayer','GTM-NWX2HBM');`
+    //   }
+    // ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/css/reset.css',
     '~/assets/css/input.css', 
+    '~/assets/css/fonts.css', 
+    '~/assets/css/general.css', 
     '~/assets/pageTransition.css',
   ],
 
