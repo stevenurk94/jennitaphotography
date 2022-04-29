76a3ad3f6fc2bf1caf42e4a10aae48cb4c0e160f
stevenurk94 <86805532+stevenurk94@users.noreply.github.com>
2022-04-29 16:46:50 +0200

    Delete components directory

--- components/icons/Mail.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <svg x="0px" y="0px" viewBox="0 0 400 400" style="enable-background:new 0 0 400 400;" xml:space="preserve">
-        <polygon id="XMLID_869_" points="0,127.5 0,274.219 104.8,206.1 	"/>
-        <polygon id="XMLID_870_" points="400,274.219 400,127.5 295.2,206.1 	"/>
-        <polygon id="XMLID_871_" points="200,277.5 130.357,225.268 0,310 0,340 400,340 400,310 269.643,225.268 	"/>
-        <polygon id="XMLID_872_" points="0,90 200,240 400,90 400,60 0,60 "/>
-    </svg>
-</template>
